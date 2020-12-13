e3e3968cfb29ebf3f962c1e39647d22ab8a2971e
boris <burbokop@gmail.com>
2020-12-14 01:09:29 +0200

    sql script added.

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,21 @@
--- Create tables.
-DROP TABLE IF EXISTS "channels";
-CREATE TABLE "channels"
-(
-    "id"   SERIAL PRIMARY KEY,
-    "name" VARCHAR(50) NOT NULL UNIQUE
+DROP TABLE IF EXISTS "virtual_mashins";
+CREATE TABLE "virtual_mashins" (
+  "id" SERIAL PRIMARY KEY,
+  "name" VARCHAR(64) NOT NULL UNIQUE,
+  "cpuCount" INT NOT NULL,
+  "discs" VARCHAR(256)
 );
 
--- Insert demo data.
-INSERT INTO "channels" (name) VALUES ('lectures');
-INSERT INTO "channels" (name) VALUES ('practice');
+DROP TABLE IF EXISTS "discs";
+CREATE TABLE "discs" (
+  "id" SERIAL PRIMARY KEY,
+  "name" VARCHAR(50) NOT NULL,
+  "diskSpace" bigint NOT NULL
+);
+
+INSERT INTO "discs" (id, name, diskSpace) VALUES (0, 'Intel', 4294967296);
+INSERT INTO "discs" (id, name, diskSpace) VALUES (1, 'Toshiba', 17179869184);
+INSERT INTO "discs" (id, name, diskSpace) VALUES (3, 'Toshiba2', 17179869184);
+
+INSERT INTO "virtual_mashins" (name, cpuCount, discs) VALUES ('vm0', 4, "0, 1");
+INSERT INTO "virtual_mashins" (name, cpuCount, discs) VALUES ('vm1', 4, "3");
