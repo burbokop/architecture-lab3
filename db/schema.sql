63654807127efa6f3113adb5f0f708a349f2170c
boris <burbokop@gmail.com>
2020-12-14 02:11:35 +0200

    db script edited

--- db/schema.sql
+++ db/schema.sql
@@ -1,21 +1,21 @@
-DROP TABLE IF EXISTS "virtual_mashins";
-CREATE TABLE "virtual_mashins" (
+DROP TABLE IF EXISTS "virtual_machines";
+CREATE TABLE "virtual_machines" (
   "id" SERIAL PRIMARY KEY,
   "name" VARCHAR(64) NOT NULL UNIQUE,
-  "cpuCount" INT NOT NULL,
-  "discs" VARCHAR(256)
+  "cpu_count" INT NOT NULL,
+  "connected_discs" VARCHAR(256)
 );
 
 DROP TABLE IF EXISTS "discs";
 CREATE TABLE "discs" (
   "id" SERIAL PRIMARY KEY,
   "name" VARCHAR(50) NOT NULL,
-  "diskSpace" bigint NOT NULL
+  "disk_space" bigint NOT NULL
 );
 
-INSERT INTO "discs" (id, name, diskSpace) VALUES (0, 'Intel', 4294967296);
-INSERT INTO "discs" (id, name, diskSpace) VALUES (1, 'Toshiba', 17179869184);
-INSERT INTO "discs" (id, name, diskSpace) VALUES (3, 'Toshiba2', 17179869184);
+INSERT INTO discs (id, name, disk_space) VALUES (0, 'Intel', 4294967296);
+INSERT INTO discs (id, name, disk_space) VALUES (1, 'Toshiba', 17179869184);
+INSERT INTO discs (id, name, disk_space) VALUES (3, 'Toshiba2', 17179869184);
 
-INSERT INTO "virtual_mashins" (name, cpuCount, discs) VALUES ('vm0', 4, "0, 1");
-INSERT INTO "virtual_mashins" (name, cpuCount, discs) VALUES ('vm1', 4, "3");
+INSERT INTO "virtual_machines" (name, cpu_count, connected_discs) VALUES ('vm0', 4, '0, 1');
+INSERT INTO "virtual_machines" (name, cpu_count, connected_discs) VALUES ('vm1', 4, '3');
